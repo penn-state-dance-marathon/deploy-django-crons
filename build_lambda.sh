3b53a440ba05a21fc464e590a589be530c94e1c8
Noah Gearhart <excelsiorvfx@gmail.com>
2021-10-04 12:32:45 -0400

    Add libfreetype

--- build_lambda.sh
+++ build_lambda.sh
@@ -23,6 +23,7 @@ cp /usr/lib64/libltdl.so* /deps
 cp /usr/lib64/libcairo* /deps
 cp /usr/lib64/libpixman* /deps
 cp /usr/lib64/libfontconfig* /deps
+cp /usr/lib64/libfreetype* /deps
 cd /deps
 zip -r9 /lambda.zip .
 
