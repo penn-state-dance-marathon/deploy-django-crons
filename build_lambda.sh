ef3ce50d4b457234278e7e91a7712498a8b93cee
Kanishk Sachdev <kanishksachdev@gmail.com>
2025-09-18 18:36:20 -0400

    downgrade to 3.9

--- build_lambda.sh
+++ build_lambda.sh
@@ -9,29 +9,28 @@
 # The variable INPUT_ZAPPA indicates whether or not this is a Zappa project so collectstatic/migrations can be run
 START_DIR=$(pwd)
 pip install -r $START_DIR/$INPUT_REQUIREMENTS
-mkdir -p /deps
+mkdir /deps
 
 # Get the Python package files and zip
-cd /usr/lib/python3.12/site-packages || cd /usr/local/lib/python3.12/site-packages
-# Skip symlinks with -y flag to avoid infinite loops
-zip -y -r9 /lambda.zip . -x "boto*" "pip*"
+cd /usr/local/lib/python3.9/site-packages
+zip -r9 /lambda.zip . -x "boto*" "pip*"
 
 # Get the C extensions files and zip them
 cp /usr/lib64/libxml* /deps
-find /usr/lib64 -name "*mariadb*" -exec cp {} /deps \;
-find /usr/lib64 -name "*mysql*" -exec cp {} /deps \;
+cp -r /usr/lib64/mysql/* /deps
 cp -r /usr/lib64/libxslt* /deps
 cp /usr/lib64/libltdl.so* /deps
 cd /deps
 zip -r9 /lambda.zip .
 
 # Zip the application files
+
 cd $START_DIR/$INPUT_CODE
 # If this isn't a Zappa deployment, exclude all static files
 if [ "$INPUT_ZAPPA" = false ]; then
-    zip -y -r9 /lambda.zip . -x "*.git*" "static/*" "*/static/*"
+    zip -r9 /lambda.zip . -x "*.git*" "static/*" "*/static/*"
 else
-    zip -y -r9 /lambda.zip . -x "*.git*"
+    zip -r9 /lambda.zip . -x "*.git*"
 fi
 
 # Include any specific files
